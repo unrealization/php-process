e7756002dfa846e8a511ca59592a40b1a4a0a9e2
Dennis Wronka <dennis@notebook.dennis>
2024-07-23 20:36:30 +0200

    Bump version number.

--- src/Process.php
+++ src/Process.php
@@ -12,7 +12,7 @@ namespace unrealization;
  * @subpackage Process
  * @link http://php-classes.sourceforge.net/ PHP Class Collection
  * @author Dennis Wronka <reptiler@users.sourceforge.net>
- * @version 3.0.0
+ * @version 4.0.0
  * @license http://www.gnu.org/licenses/old-licenses/lgpl-2.1.html LGPL 2.1
  */
 class Process
